68f2a0dd842d284233cb9504a00f46c0a5f5f2a3
kkito0726 <k.kito0726@gmail.com>
2023-01-30 14:49:34 +0000

    springbootの雛形を作製

---
+++ simple-spring-rest-api/src/test/java/com/example/simplespringrestapi/SimpleSpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplespringrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
