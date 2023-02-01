596473f8682243aa276dcb633d9038293abce239
kkito0726 <k.kito0726@gmail.com>
2023-02-01 14:33:25 +0000

    オートインクリメントとレスポンスステータスを返すようにした

--- simple-spring-rest-api/src/main/java/com/example/simplespringrestapi/controllers/TodoController.java
+++ simple-spring-rest-api/src/main/java/com/example/simplespringrestapi/controllers/TodoController.java
@@ -6,11 +6,16 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.simplespringrestapi.models.TodoItem;
 
@@ -21,14 +26,15 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 @RequestMapping(path = TodoController.BASE_URL)
 public class TodoController {
   public static final String BASE_URL = "/api/v1/todos";
+  private final AtomicInteger _counter = new AtomicInteger();
 
   // すべてのtodoをためておくリスト
   private final ArrayList<TodoItem> _todoItems = new ArrayList<>() {
     {
-      add(new TodoItem(1, "todo 1"));
-      add(new TodoItem(2, "todo 2"));
-      add(new TodoItem(3, "todo 3"));
-      add(new TodoItem(4, "todo 4"));
+      add(new TodoItem(_counter.incrementAndGet(), "todo 1"));
+      add(new TodoItem(_counter.incrementAndGet(), "todo 2"));
+      add(new TodoItem(_counter.incrementAndGet(), "todo 3"));
+      add(new TodoItem(_counter.incrementAndGet(), "todo 4"));
     }
   };
 
@@ -56,36 +62,49 @@ public class TodoController {
   @RequestMapping(method = RequestMethod.GET, path = "/{id}")
   public TodoItem getTodoItem(@PathVariable int id) {
     TodoItem found = getTodoItemById(id);
+    if (found == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not Found");
+    }
     return found;
   }
 
   // create todo
   @RequestMapping(method = RequestMethod.POST, path = "")
-  public TodoItem createTodoItems(@RequestBody TodoItem todoItem) {
-    todoItem.setId(4);
+  public ResponseEntity<?> createTodoItems(@RequestBody TodoItem todoItem) {
+    todoItem.setId(_counter.incrementAndGet());
     _todoItems.add(todoItem);
-    return todoItem;
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(todoItem.getId()).toUri();
+    
+    return ResponseEntity.created(location).body(todoItem);
   }
 
   // update todo
   @RequestMapping(method = RequestMethod.PUT, path = "/{id}")
-  public TodoItem putTodoItems(@PathVariable int id, @RequestBody TodoItem todoItem) {
+  public ResponseEntity<?> updateTodoItems(@PathVariable int id, @RequestBody TodoItem todoItem) {
     TodoItem found = getTodoItemById(id);
+
+    if (found == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not Found");
+    }
 
     _todoItems.remove(found);
     _todoItems.add(todoItem);
 
-    return todoItem;
+    return ResponseEntity.noContent().build();
   }
 
   // delete todo
   @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
-  public String deleteTodoItems(@PathVariable int id) {
+  public ResponseEntity<?> deleteTodoItems(@PathVariable int id) {
     TodoItem found = getTodoItemById(id);
 
     _todoItems.remove(found);
 
-    return "delete todo item" + "id=" + id;
+    if (found == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not Found");
+    }
+
+    return ResponseEntity.noContent().build();
   }
 
   private TodoItem getTodoItemById(int id) {
